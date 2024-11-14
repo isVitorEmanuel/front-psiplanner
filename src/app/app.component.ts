856fcd44b66cafa872852c93b1ff1447eaf6eea2
Igor Marques <igor.marques.121@ufrn.edu.br>
2024-11-13 21:37:59 -0300

    add a static navbar, dynamic implementation pending

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "./components/button/button.component";
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ButtonComponent, RouterOutlet],
+  imports: [ButtonComponent, RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
